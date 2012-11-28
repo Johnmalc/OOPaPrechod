41709273772b9edf5ccd4999e50f0cf119bc696b
Johnmalc <cincenko@seznam.cz>
2012-11-28 22:48:27 +0100

    ipdate

--- src/ubung2711Sam/Berechnung.java
+++ src/ubung2711Sam/Berechnung.java
@@ -2,14 +2,14 @@ package ubung2711Sam;
 
 public class Berechnung {
 
-	public double kosten(Flugzeug tmp) {
-		double ko = (tmp.cenaPristani()*2 + (crewPreis * 2)
+	public double getKosten(int cenaPristani, int anzahlPersonen, int crewPreis, int CenaPaliva, int CenaProPerson ) {
+		double ko = (cenaPristani*2 + crewPreis * 2) // cesta tam a zpet
 				+ (anzahlPersonen * CenaPaliva * CenaProPerson)
 				+ (anzahlPersonen * 0.8 * CenaPaliva * CenaProPerson);
 		return ko;
 	}
-	public double gewinn(int ticketPreis, int anzahlPersonen) {
-		double ge = ticketPreis*anzahlPersonen;
+	public double getGewinn(int ticketPreis, int anzahlPersonen) {
+		double ge = (ticketPreis*anzahlPersonen) + 0.8*(ticketPreis*anzahlPersonen);
 		return ge;
 	}
 
--- src/ubung2711Sam/Main.java
+++ src/ubung2711Sam/Main.java
@@ -6,9 +6,17 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Flugzeug Spider = new Flugzeug(3000000,130,9.6);
-		Berechnung Spider2 = new Berechnung();
-		System.out.println(Spider2.kosten(499, 8990, 130, 9.6, 9.33));
+		Flugzeug Spider = new Flugzeug(3000000, 130, 9.6);
+		int passanger = Spider.getAnzahlPersonen();
+		int cenapristani = Spider.cenaPristani();
+		int crew= Spider.crewPreis();
+		int cenapaliva = Spider.
+		nt CenaPaliva, int CenaProPerson
+		Berechnung Spider2 = new Berechnung(cenapristani, passanger,crew);
+		 System.out.println(Spider.getKosten());
+		// System.out.println((((499, 8990, 130, 9.6, 9.33));
+
+		// gewinn tam a zpet
 	}
 
 }
