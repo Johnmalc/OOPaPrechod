36a3898e4a43d11697a7e91bbab6729e6fbad3b6
johnmalc <cincenko@seznam.cz>
2013-01-08 19:24:21 +0100

    o801

---
+++ src/ubung0801/Schiff.java
@@ -0,0 +1,35 @@
+/**
+  *
+  * Beschreibung
+  *
+  * @version 1.0 vom 08.01.2013
+  * @author
+  */
+
+public class Schiff {
+  Object[] d;
+  int zahler;
+
+  public Schiff (int grosse){
+    d= new Object [grosse];
+    zahler=0;
+    
+  }
+  
+  public void add(Object a) {
+    d[zahler] = a;
+    zahler++;
+    
+  }
+  public Baum getBuch(){
+    
+    for (int i=0;i<d.length ;i++ ) {
+       if(d[i] instanceof Baum){
+         Baum Bx =  (Baum) d[i];
+         return Bx;
+       }
+    } // end of for
+    return null;
+  }
+
+} // end of class Schiff
